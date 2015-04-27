efd0ff2de899ac3282544350c004c7afac1745d9
Ben <shatterwaltz@gmail.com>
2015-04-27 15:37:04 -0400

    Finished making code not complete shit

--- code/LogicParser.java
+++ code/LogicParser.java
@@ -17,11 +17,11 @@ public class LogicParser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         System.out.println("& - and\n/ - or\n! - not\n> - implies\nInput must be fully disambiguated with parentheses.\nUse only one letter for variables.\n*******************\nEnter well formed formula:\n");
         Scanner s = new Scanner(System.in);
         Parser p = new Parser(s.nextLine());
         System.out.println("I interpreted: " + p.getWff());
-        for(int i=0;i<p.getVarList().size();i++)
-            System.out.println(p.getVarList().get(i));
+        
     }
 }
--- code/Parser.java
+++ code/Parser.java
@@ -11,86 +11,68 @@ import java.util.ArrayList;
  */
 public class Parser {
     private Statement wff;
+    int position=0;
     static ArrayList<Variable> varlist = new ArrayList(0);
     public Parser(String data){
-        wff=parse(data);
+       wff=parse(data);
     }
     
     public Statement getWff(){return wff;}
+    
     private Statement parse(String input){
-    int pos=0;
-    int parencount=0;
-    char cur;
-    cur=input.charAt(pos);
-        do{
+        position = 0;
+        char cur;
+        cur=input.charAt(position);
         if(cur=='('){
-            String temp1=parenParser(input, pos);
-            if(pos<input.length()){
-                pos++;
-                do{
-                    cur=input.charAt(pos);
-                    if(cur=='(')
-                        parencount++;
-                    else if(cur==')')
-                        parencount--;
-                    pos++;
-                    
-                }while(parencount>0);
-                pos++;
-                cur=input.charAt(pos);
-                char operator = cur;
-                String temp2 = parenParser(input, pos);
-                System.out.println(temp1+","+temp2);
-                if(operator=='&'){
-                    return new And(parse(temp1), parse(temp2));
-                }
-                else if(operator=='/'){
-                    return new Or(parse(temp1), parse(temp2));
-                }
-                else if(operator=='>'){
-                    return new Implies(parse(temp1), parse(temp2));
-                }
+            position++;
+            String temp1=parenParser(input.substring(position));
+            if(position>=input.length()){
+                return parse(temp1);
             }
-            
+            else{
+                char operator = input.charAt(position);
+                position+=2;
+                String temp2=parenParser(input.substring(position));
+                switch(operator){
+                    case '&':
+                        return new And(parse(temp1), parse(temp2));
+                    case '/':
+                        return new Or(parse(temp1), parse(temp2));
+                    case '>':
+                        return new Or(new Not(parse(temp1)), parse(temp2));
+                }
+                        
+                        
+            }
+        }else if(cur=='!'){
+            position+=2;
+            return new Not(parse(parenParser(input.substring(position))));
+        }else{
+            return new Variable(cur);
         }
-        if(cur=='!'){
-            return new Not(parse(parenParser(input, pos)));
-        }
-        else{
-            //Check if variable already exists in varlist. If not, add to list.
-            //boolean varExists=false;
-            Variable v=new Variable(cur);
-            
-            return v;
-        }
-        }while(pos<input.length());
-    }
         
-    //Parses through substrings encased in () or whatever
-    //Looks like it can handle things like ((~~~)~~) too which is cool
-    private String parenParser(String input, int pos){
-        int parenCount=0;
-        char cur=input.charAt(pos);
-        String temp="";
-            pos++;
-            do{
-                cur=input.charAt(pos);
-                if(cur=='('){
-                    parenCount++;
-                    if(parenCount!=1)
-                        temp+=cur;
-                }
-                else if(cur==')'){
-                    parenCount--;
-                    if(parenCount!=0)
-                        temp+=cur;
-                }
-                else
-                    temp+=cur;
-                pos++;
-            }while(parenCount>0);
+        
+        return null;
+    }
+    private String parenParser(String input){
+        int parencount=1;
+        //tracks parenparser's progress through the substring
+        int parenpos=0;
+        char cur;
+        do{
+            cur=input.charAt(parenpos);
+            if(cur=='(')
+                parencount++;
+            else if(cur==')')
+                parencount--;
             
-        return temp;
+            parenpos++;
+        }while(parencount>0);
+        //shifts the parser's position marker over by how much was processed by parenparser
+       
+       
+        position+=parenpos;
+        return input.substring(0, parenpos-1);
     }
     public ArrayList<Variable> getVarList(){return varlist;}
 }
