6bc34fae61308c80f8ec2a3092efe946cd9d2b05
Nottapesahc <Nottapesahc@gmail.com>
2015-07-09 01:54:18 -0400

    updated with proper folder names for execution in terminal.

---
+++ logicparser/Variable.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicparser;
+
+/**
+ * Evaluated in all combinations of false/true with other variables.
+ * Modified by other statements such as Not, And, or Implies.
+ * @author shatterwaltz
+ */
+public class Variable implements Statement{
+    private boolean value=false;
+    private String name="";
+    public Variable(char name){
+        this.name+=name;
+    }
+    public void switchVal(){
+        if(value)
+            value=false;
+        else
+            value=true;
+    }
+    public void setVal(boolean newVal){value=newVal;}
+    
+    public boolean evaluate(){return value;}
+    public String toString(){
+        return name;
+    }
+    public String getName(){return name;}
+}
